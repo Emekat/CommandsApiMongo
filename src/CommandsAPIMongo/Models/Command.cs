b5d156fb6f8c0520ccabc826a3d8d220f07c83b8
Emeka Torti <etorti@questrade.com>
2021-09-06 16:19:59 -0400

    Added Command Model

---
+++ src/CommandsAPIMongo/Models/Command.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CommandsAPIMongo.Models
+{
+    public class Command
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
+
