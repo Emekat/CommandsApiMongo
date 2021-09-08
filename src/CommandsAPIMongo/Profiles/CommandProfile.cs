d5c9e405fd47f1d4d0ab3adeb873706f46d35ea6
Emeka Torti <etorti@questrade.com>
2021-09-08 16:50:15 -0400

    Added DTO classes and setup Automapper

---
+++ src/CommandsAPIMongo/Profiles/CommandProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CommandsAPIMongo.Dtos;
+using CommandsAPIMongo.Models;
+
+namespace CommandsAPIMongo.Profiles
+{
+    public class CommandProfile:Profile
+    {
+        public CommandProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+            CreateMap<Command, CommandUpdateDto>();
+        }
+    }
+}
--- src/CommandsAPIMongo/Startup.cs
+++ src/CommandsAPIMongo/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CommandsAPIMongo.Data;
 using MongoDB.Driver;
 
@@ -31,6 +25,7 @@ namespace CommandsAPIMongo
 
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo, CommandAPIRepo>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSingleton<IMongoClient, MongoClient>(sp =>
                 new MongoClient(Configuration.GetConnectionString("MongoDbConnectionString")));
 
